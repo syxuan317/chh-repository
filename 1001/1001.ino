19cd44468b19830b4ecdddee313ce62eb44e1414
syxuan317 <p5779661@gmail.com>
2025-10-01 14:52:15 +0800

    1001

---
+++ 1001/1001.ino
@@ -0,0 +1,69 @@
+const int ButtonPin = 2;
+const int RledPin = 3;
+const int GledPin = 4;
+const int BledPin = 5;
+
+int buttonState = 0;
+int ledcolor = 0;
+
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(RledPin, OUTPUT);
+  pinMode(GledPin, OUTPUT);
+  pinMode(BledPin, OUTPUT);
+  pinMode(ButtonPin, INPUT);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  buttonState = digitalRead(ButtonPin);
+
+  if(buttonState == HIGH){
+    ledcolor = ledcolor+1;
+    delay(100);
+  }
+  if(ledcolor == 0){
+    digitalWrite(RledPin, HIGH);
+    digitalWrite(BledPin, HIGH);
+    digitalWrite(GledPin, HIGH);
+  }
+  else if(ledcolor == 1){
+    digitalWrite(RledPin, LOW);
+    digitalWrite(BledPin, HIGH);
+    digitalWrite(GledPin, HIGH);
+  }
+  else if(ledcolor == 2){
+    digitalWrite(RledPin, HIGH);
+    digitalWrite(BledPin, LOW);
+    digitalWrite(GledPin, HIGH);
+  }
+  else if(ledcolor == 3){
+    digitalWrite(RledPin, HIGH);
+    digitalWrite(BledPin, HIGH);
+    digitalWrite(GledPin, LOW);
+  }
+  else if(ledcolor == 4){
+    digitalWrite(RledPin, LOW);
+    digitalWrite(BledPin, LOW);
+    digitalWrite(GledPin, HIGH);
+  }
+  else if(ledcolor == 5){
+    digitalWrite(RledPin, LOW);
+    digitalWrite(BledPin, HIGH);
+    digitalWrite(GledPin, LOW);
+  }
+  else if(ledcolor == 6){
+    digitalWrite(RledPin, HIGH);
+    digitalWrite(BledPin, LOW);
+    digitalWrite(GledPin, LOW);
+  }
+  else if(ledcolor == 7){
+    digitalWrite(RledPin, LOW);
+    digitalWrite(BledPin, LOW);
+    digitalWrite(GledPin, LOW);
+  }
+  else if(ledcolor == 8){
+    ledcolor = 0;
+  }
+
+}
